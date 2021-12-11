82003a7976c0438546c774efccff3749418899b9
eddykram <eddy.kram@gmail.com>
2021-12-11 13:14:09 -0500

    shop page update

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var headphonesColor = $(this).attr('.data-image');
-
-        $('.active').removeClass('active');
-        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-        $(this).addClass('active');
-
-});
-
