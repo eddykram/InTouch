95469e1f9d343433b52a0bd39f94eb20c22cb567
eddykram <eddy.kram@gmail.com>
2021-12-14 17:17:52 -0500

    force push

--- js/shop.js
+++ js/shop.js
@@ -21,5 +21,5 @@ function resetActiveBtns() {
 // set new color on the banner right 
 function setNewColor(color) {
 
-    document.querySelector('.banner-right img').src = `Images/intouch-${color}.png`;
+    document.querySelector('.banner-right img').src = `Images/intouch-${color}.PNG`;
 }
